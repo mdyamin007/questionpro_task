d203656820f5df8c95d9da3b7d23a857f33f4b5a
mdyamin007 <mohammadyamin80@gmail.com>
2025-09-07 23:31:39 +0600

    feat: select option dropdown implemented for comments

--- src/pages/CommentsPage.tsx
+++ src/pages/CommentsPage.tsx
@@ -1,20 +1,51 @@
 import { useQuery } from "@tanstack/react-query";
 import ListWithDropDown from "../components/common/ListWithDropDown";
 import fetchComments from "../api/fetchComments";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import type { Comment } from "../types/data.type";
+import fetchPosts from "../api/fetchPosts";
+import { MySelect, type Option } from "../components/ui/MySelect";
 
 function CommentsPage() {
-  const [postId] = useState<number>(1);
+  const [postId, setPostId] = useState<number>(1);
 
   const { data: comments } = useQuery({
     queryKey: ["comments", postId],
     queryFn: () => fetchComments(postId),
   });
 
+  const { data: posts } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(),
+  });
+
+  const postOptions: Option[] = useMemo(() => {
+    return (
+      posts?.map((p) => ({
+        label: p.title,
+        value: p.id.toString(),
+      })) || []
+    );
+  }, [posts]);
+
+  const handlePostChange = (value: string) => {
+    setPostId(Number(value));
+  };
+
   return (
     <>
-      <ListWithDropDown<Comment> title="Comments" data={comments || []} />
+      <ListWithDropDown<Comment>
+        title="Comments"
+        data={comments || []}
+        selectDropdown={
+          <MySelect
+            options={postOptions}
+            onChange={handlePostChange}
+            placeholder="Select a post"
+            emptyText="No posts found"
+          />
+        }
+      />
     </>
   );
 }
